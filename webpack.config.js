cae8824730d6bba554b5d7d9c01d7642084edcd4
Xinhe <xinhe998@gmail.com>
2019-09-23 10:36:46 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     output: {
         //將輸出的檔案放到這個資料夾下
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         //將所有依賴的模組都合併輸出到這個檔案
         filename: 'index_bundle.js',
         publicPath: '/'
